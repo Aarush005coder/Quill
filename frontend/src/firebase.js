5a3f20b93b0d16ba2410eadb977052ad5e5f5e4a
Aarush Khandelwal <khandelwalaarush2@gmail.com>
2026-08-26 00:58:24 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7K8ZdTY3V2_gUZO-3JkAkS7wFSbc1JWg",
+  apiKey: "AIzaSyD7*********3JkAkS7wFSbc1JWg",
   authDomain: "abcd-b6381.firebaseapp.com",
-  projectId: "abcd-b6381",
+  projectId: "ab****81",
   storageBucket: "abcd-b6381.firebasestorage.app",
-  messagingSenderId: "207191904504",
-  appId: "1:207191904504:web:434f6b0bea51491e076b0d",
-  measurementId: "G-0DSE7P7DN0"
+  messagingSenderId: "20******4504",
+  appId: "1:207191904504:web:434f6*****91e076b0d",
+  measurementId: "G-0D****7DN0"
 };
 
 // Initialize Firebase
