48b8404d797e20d4a80046b413884791723c82bf
Aarush Khandelwal <khandelwalaarush2@gmail.com>
2026-08-27 23:22:13 +0530

    Add createcachetable to build script

--- backend/build.sh
+++ backend/build.sh
@@ -5,4 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+# Cache table banao (agar nahi hai toh)
+python manage.py createcachetable || true
+
 python manage.py migrate
